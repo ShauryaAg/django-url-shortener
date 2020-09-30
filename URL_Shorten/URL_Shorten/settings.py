4573059ed517703082ef6a01f2492055e251a9f7
ShauryaAg <agarwal.lucky.shaurya11@gmail.com>
2020-09-30 23:10:09 +0530

    changes

--- URL_Shorten/URL_Shorten/settings.py
+++ URL_Shorten/URL_Shorten/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,10 @@ SECRET_KEY = '9^vs0!-&rzn=c#^%59ze57wmtk47(khaifd+y6cqn%%lt*8wx9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+                    'http://uri-short.herokuapp.com/',
+                    os.getenv('PROJECT_NAME') + ".glitch.me"
+]
 
 
 # Application definition
@@ -117,5 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
