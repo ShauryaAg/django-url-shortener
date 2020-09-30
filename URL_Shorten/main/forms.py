bbd0ece697b44c30d66c0da57b8070f4515b7da8
ShauryaAg <agarwal.lucky.shaurya11@gmail.com>
2020-10-01 04:05:10 +0530

    changes

--- URL_Shorten/main/forms.py
+++ URL_Shorten/main/forms.py
@@ -1,15 +1,23 @@
 from django import forms
 from .models import short_urls
+from django.utils.translation import ugettext_lazy as _
 
 
 class UrlForm(forms.ModelForm):
 
-    short_url = forms.CharField(required=False)
+    # short_url = forms.CharField(required=False, label='Token')
 
     class Meta:
         model = short_urls
-        fields = ['short_url', 'long_url']
-        widgets = {
-            'short_url': forms.TextInput(attrs=({'class': 'url-input'})),
-            'long_url': forms.TextInput(attrs=({'class': 'url-input'}))
+        fields = ('short_url', 'long_url')
+        labels = {
+            'short_url': _('Token'),
         }
+        widgets = {
+            'long_url': forms.TextInput(attrs=({'class': 'url-input'})),
+            'short_url': forms.TextInput(attrs=({'class': 'url-input'}))
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UrlForm, self).__init__(*args, **kwargs)
+        self.fields['short_url'].required = False
